d0ce9a998cebec88f7c53094a32bc0e9b24d54c4
R Thurimella <rthurimella@yahoo.com>
2023-11-09 16:22:45 -0700

    Add all the functionality. (Note: Some html files in will show errors in the Community Edition of Intellij. Ignore that. The app builds and runs just fine.)

--- src/main/java/com/application/courseLibrary/controller/IndexController.java
+++ src/main/java/com/application/courseLibrary/controller/IndexController.java
@@ -1,4 +1,10 @@
 package com.application.courseLibrary.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class IndexController {
+    @GetMapping("/")
+    public String listHome(){
+        return "index";
+    }
 }
